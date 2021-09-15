d2ad3263b58f99550e3fdc9c6dd58c8e7a86b8c9
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-09-15 08:06:58 +0530

    made some changes

--- js/background.js
+++ js/background.js
@@ -1,3 +1,11 @@
-chrome.runtime.onInstalled.addListener(() => {
-	console.log("Hello");
+chrome.runtime.onInstalled.addListener((details) => {
+	if(details.reason === 'install') {
+		// Do Something
+	} else if (details.reason === "update") {
+		// Do Something
+	} else if (details.reason === "chrome_update") {
+		// Do Something
+	} else if (details.reason === "shared_module_update") {
+		// Do Something
+	};
 });
