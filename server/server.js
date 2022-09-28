86a7ebfd79a38bca6269903649cd7da3118617c2
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-09-28 18:35:45 +0530

    the server is here!!

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.post("/webhook", (req, res) => {
+    const data = req.body;
+
+    console.log(data);
+    res.status(200).send("Response recieved!!");
+});
+
+const server = app.listen(3000, () => {
+    console.log("Running on http://localhost:%s", server.address().port);
+});
