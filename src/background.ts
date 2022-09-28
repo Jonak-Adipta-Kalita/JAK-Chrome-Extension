72273ed445adb780c1dc8241903c2bccf92a33d2
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-09-28 14:39:00 +0530

    now how can i send a message everytime there is a new release!!

--- src/background.ts
+++ src/background.ts
@@ -2,7 +2,6 @@ chrome.runtime.onInstalled.addListener((details) => {
     const name = "JAK Chrome Extension";
 
     if (details.reason === "install") {
-        console.log(`Thanks for Installing ${name}!!`);
         chrome.notifications.create("install", {
             type: "basic",
             iconUrl: "../../assets/images/logo.png",
@@ -11,7 +10,6 @@ chrome.runtime.onInstalled.addListener((details) => {
             priority: 2,
         });
     } else if (details.reason === "update") {
-        console.log(`Thanks for Updating ${name}!!`);
         chrome.notifications.create("update", {
             type: "basic",
             iconUrl: "../../assets/images/logo.png",
@@ -20,7 +18,6 @@ chrome.runtime.onInstalled.addListener((details) => {
             priority: 2,
         });
     } else if (details.reason === "chrome_update") {
-        console.log(`Please also Update ${name} to get the most values!!`);
         chrome.notifications.create("chrome_update", {
             type: "basic",
             iconUrl: "../../assets/images/logo.png",
@@ -29,7 +26,6 @@ chrome.runtime.onInstalled.addListener((details) => {
             priority: 2,
         });
     } else if (details.reason === "shared_module_update") {
-        console.log(`A Shared Module is Updated!!`);
         chrome.notifications.create("shared_module_update", {
             type: "basic",
             iconUrl: "../../assets/images/logo.png",
