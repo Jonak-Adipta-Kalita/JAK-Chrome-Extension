f6ab7aa2bfb2da7b0f51bcfc4174ca2062e232d0
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-10-02 22:31:35 +0530

    the console.log is not appearing for the onMessage

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+    (this && this.__importDefault) ||
+    function (mod) {
+        return mod && mod.__esModule ? mod : { default: mod };
+    };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app_1 = require("firebase/app");
@@ -10,17 +12,18 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3000;
-const firebaseApp = (0, app_1.getApps)().length === 0
-    ? (0, app_1.initializeApp)({
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
-        measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-    })
-    : (0, app_1.getApp)();
+const firebaseApp =
+    (0, app_1.getApps)().length === 0
+        ? (0, app_1.initializeApp)({
+              apiKey: process.env.FIREBASE_API_KEY,
+              authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+              projectId: process.env.FIREBASE_PROJECT_ID,
+              storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+              messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+              appId: process.env.FIREBASE_APP_ID,
+              measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+          })
+        : (0, app_1.getApp)();
 const messaging = (0, messaging_1.getMessaging)(firebaseApp);
 console.log(messaging);
 app.use(express_1.default.json());
@@ -33,8 +36,7 @@ app.post("/webhook", (req, res) => {
     if (data.action === "released") {
         console.log(data);
         res.status(200).send("Response recieved!!");
-    }
-    else {
+    } else {
         res.status(200).send("Response recieved but not the one expected!!");
     }
 });
