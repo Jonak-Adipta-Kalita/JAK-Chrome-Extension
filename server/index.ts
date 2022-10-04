3707e9491b9274a91c130732442725764823ee07
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-10-04 21:03:36 +0530

    i have to use a different approach

--- server/index.ts
+++ server/index.ts
@@ -1,28 +1,10 @@
 import express from "express";
 import { Release } from "github-webhook-event-types";
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getMessaging } from "firebase/messaging";
 import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-const firebaseApp =
-    getApps().length === 0
-        ? initializeApp({
-              apiKey: process.env.FIREBASE_API_KEY,
-              authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-              projectId: process.env.FIREBASE_PROJECT_ID,
-              storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-              messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-              appId: process.env.FIREBASE_APP_ID,
-              measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-          })
-        : getApp();
-
-const messaging = getMessaging(firebaseApp);
-console.log(messaging);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
