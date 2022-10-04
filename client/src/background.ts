56c26917534691f8c6b5f3ad9315943601f9dbd7
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-10-04 20:42:41 +0530

    well.... firebase messaging is not supported??

--- client/src/background.ts
+++ client/src/background.ts
@@ -1,3 +1,6 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
+
 try {
     chrome.runtime.onInstalled.addListener((details) => {
         const name = "JAK Chrome Extension";
@@ -35,6 +38,33 @@ try {
                 priority: 2,
             });
         }
+    });
+
+    const firebaseApp =
+        getApps().length === 0
+            ? initializeApp({
+                  apiKey: process.env.FIREBASE_API_KEY,
+                  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+                  projectId: process.env.FIREBASE_PROJECT_ID,
+                  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+                  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+                  appId: process.env.FIREBASE_APP_ID,
+                  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+              })
+            : getApp();
+
+    let firebaseToken: string;
+
+    const messaging = getMessaging(firebaseApp);
+
+    getToken(messaging, {
+        vapidKey: process.env.FIREBASE_CLOUD_MESSAGING_VAPID_KEY,
+    }).then((token) => {
+        firebaseToken = token;
+    });
+
+    onMessage(messaging, (payload) => {
+        console.log(payload);
     });
 } catch (error) {
     console.log(error);
