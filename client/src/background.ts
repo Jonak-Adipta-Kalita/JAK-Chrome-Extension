88eeeb69eb1b473f59dc7e0c66d70b9afb107be8
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-10-06 09:25:21 +0530

    we are gonna do the fetching stuff in the server

--- client/src/background.ts
+++ client/src/background.ts
@@ -1,6 +1,4 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
 import { Release as Release_ } from "github-webhook-event-types";
-import { get, getDatabase, ref, onChildAdded } from "firebase/database";
 
 interface Release extends Release_ {
     timestamp: Object;
@@ -43,36 +41,6 @@ try {
                 priority: 2,
             });
         }
-    });
-
-    const firebaseApp =
-        getApps().length === 0
-            ? initializeApp({
-                  apiKey: process.env.FIREBASE_API_KEY,
-                  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-                  projectId: process.env.FIREBASE_PROJECT_ID,
-                  databaseURL: process.env.FIREBASE_DATABASE_URL,
-                  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-                  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-                  appId: process.env.FIREBASE_APP_ID,
-                  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-              })
-            : getApp();
-
-    const db = getDatabase(firebaseApp);
-
-    const docRef = ref(db, "notification");
-
-    onChildAdded(docRef, () => {
-        get(docRef).then((docSnapshot) => {
-            if (docSnapshot.exists()) {
-                const data = docSnapshot.val() as Release;
-
-                console.log(data);
-                // Display Notification
-                // Delete the Data
-            }
-        });
     });
 } catch (error) {
     console.log(error);
--- server/index.js
+++ server/index.js
@@ -4,27 +4,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const app_1 = require("firebase/app");
-const database_1 = require("firebase/database");
+const firebase_admin_1 = __importDefault(require("firebase-admin"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3000;
-const firebaseApp = (0, app_1.getApps)().length === 0
-    ? (0, app_1.initializeApp)({
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
+const firebaseApp = firebase_admin_1.default.apps.length === 0
+    ? firebase_admin_1.default.initializeApp({
+        credential: firebase_admin_1.default.credential.cert({
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+            projectId: process.env.FIREBASE_PROJECT_ID,
+        }),
         databaseURL: process.env.FIREBASE_DATABASE_URL,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
-        measurementId: process.env.FIREBASE_MEASUREMENT_ID,
     })
-    : (0, app_1.getApp)();
-const db = (0, database_1.getDatabase)(firebaseApp);
+    : firebase_admin_1.default.app();
+const db = firebase_admin_1.default.database(firebaseApp);
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, cors_1.default)({ origin: true }));
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
@@ -32,13 +31,17 @@ app.post("/webhook", (req, res) => {
     const data = req.body;
     if (data.action === "released") {
         try {
-            const docRef = (0, database_1.ref)(db, "notification");
-            (0, database_1.get)(docRef).then((snapshot) => {
-                const currentNotificaions = snapshot.val();
-                const newNotification = Object.assign(Object.assign({}, data), { timestamp: (0, database_1.serverTimestamp)() });
-                (0, database_1.set)(docRef, currentNotificaions
-                    ? currentNotificaions.push(newNotification)
-                    : [newNotification]);
+            const docRef = db.ref("notifications");
+            docRef.get().then((snapshot) => {
+                const newNotification = Object.assign(Object.assign({}, data), { timestamp: firebase_admin_1.default.database.ServerValue.TIMESTAMP });
+                if (!snapshot.exists()) {
+                    docRef.set([newNotification]);
+                }
+                else {
+                    const currentNotificaions = snapshot.val();
+                    currentNotificaions.push(newNotification);
+                    docRef.set(currentNotificaions);
+                }
             });
             res.status(200).send("Response recieved!!");
         }
