f68904bcbd71b2cb1bb32449412cf879172fb480
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-10-03 16:13:47 +0530

    function component is always best!

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,21 +1,7 @@
-import { useReducer } from "react";
-
 import "./style.css";
 
-function IndexPopup() {
-    const [count, increase] = useReducer((c) => c + 1, 0);
-
-    return (
-        <button
-            onClick={() => increase()}
-            type="button"
-            className="inline-flex items-center rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Count:
-            <span className="ml-2 inline-flex h-4 w-8 items-center justify-center rounded-full bg-blue-200 text-xs font-semibold text-blue-800">
-                {count}
-            </span>
-        </button>
-    );
-}
+const IndexPopup = () => {
+    return <div></div>;
+};
 
 export default IndexPopup;
