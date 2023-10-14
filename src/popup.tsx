8f50d89b8825529dccfd51d53b7397b52608aac0
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-10-14 08:39:41 +0530

    lets do the header real quick

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,7 +1,13 @@
 import "./style.css";
 
+import Header from "~components/Header";
+
 const IndexPopup = () => {
-    return <div></div>;
+    return (
+        <div>
+            <Header />
+        </div>
+    );
 };
 
 export default IndexPopup;
