b3bd328b4a5b2e37706d6b04aa7debb1603775be
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-10-16 20:05:55 +0530

    something fr feels odd!

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@ import logo from "data-base64:~assets/icon.png";
 
 const Header = () => {
     return (
-        <div className="flex items-center justify-between px-[15px] py-[9px]">
+        <div className="flex items-center justify-between px-[15px] py-[12px]">
             <div className="flex items-center space-x-4">
-                <img src={logo} alt="Logo" className="h-[50px] w-[50px]" />
+                <img src={logo} alt="Logo" className="h-[40px] w-[40px]" />
                 <p className="text-center text-2xl font-[700] leading-[0px] text-[#F4F3F3]">
                     StreamlineX
                 </p>
